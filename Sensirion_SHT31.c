aed67da86c444c096165577cf119dd98ed1d58b0
Dieter Vansteenwegen <48558992+hypermaq@users.noreply.github.com>
2020-02-17 14:01:19 +0100

    - v0.2 of Sensirion SHT31 library

--- Sensirion_SHT31.c
+++ Sensirion_SHT31.c
@@ -1,19 +1,32 @@
+/*
+ * File:   Sensirion_SHT31.c
+ * Author: dieterv
+ * v0.2
+ * Created on February 3, 2020, 3:09 PM
+ */
+
 #include <xc.h>
-#include "I2C1.h"
 #define    FCY    6000000UL    // Instruction cycle frequency, Hz - required for __delayXXX() to work
 #include <libpic30.h>          // For delay functions
 #include "Sensirion_SHT31.h"
+#include "stdint.h"
+#include "I2C1.h"
 
-const char SHT31_Address = 0x45;
+// Constants (address and commands):
+const uint8_t SHT31_Address = 0x45;
 // constants with commands are in format:
 // {int number of bytes, byte1, byte2}
-const char CMD_SoftReset[3] = {2, 0x30, 0xA2};
-const char CMD_SingleShot[3] = {2, 0x2C, 0x06};
+const uint8_t CMD_SoftReset[3] = {2, 0x30, 0xA2};
+const uint8_t CMD_SSHighRep[3] = {2, 0x2C, 0x06};
+const uint8_t CMD_SSMidRep[3] = {2, 0x2C, 0x0D};
+const uint8_t CMD_SSLowRep[3] = {2, 0x2C, 0x10};
+const uint8_t CMD_HeaterOn[3] = {2, 0x30, 0x6D};
+const uint8_t CMD_HeaterOff[3] = {2, 0x30, 0x66};
 
 
 
 
-int SHT31_InitReset(void){
+int8_t SHT31_InitReset(void){
     // check if device is on bus and responding
     if(I2C1_PollDevice(SHT31_Address) < 0){
         return -1;
@@ -22,7 +35,7 @@ int SHT31_InitReset(void){
     // send soft reset command, this resets all config and loads calibration
     if(I2C1_Write(  SHT31_Address,\
                     0xFF,\
-                    (char*)&CMD_SoftReset[1],\
+                    (uint8_t *)&CMD_SoftReset[1],\
                     CMD_SoftReset[0]\
                     ) <0){
         return -1;
@@ -31,59 +44,165 @@ int SHT31_InitReset(void){
     }
 }
 
-int SHT31_SingleShot(signed int *buffer){
-    unsigned char MeasurementData[6];
+int8_t SHT31_SingleShot(int16_t * pTempResult, uint8_t * pRHResult, uint8_t Repeatability){
+    // High level function
+    // Reads out sensor, checks both CRC's, 
+    // Saves (temp *100) in *pTempResult, (int16_t)
+    // Saves HR in *pRHResult (uint8_t)
+    // Returns 1 if all went well
+    // Returns 0 if not all went well
+    uint8_t MeasurementData[6] = {0};           // buffer for the raw measurement data
     
-    // address device and start measurement     // intermediary buffer for data
+    if (SHT31_GetData(MeasurementData, Repeatability) < 1){
+        return -1;
+    }
+    // check CRCs on data
+    if (SHT31_CheckCRC(MeasurementData) < 1){            // Temp CRC
+        return -2;                                       // error checking CRC
+    }
+    if (SHT31_CheckCRC(&MeasurementData[3] ) < 1){       // RH CRC
+        return -2;                                       // error checking CRC
+    }
+    
+    // calculate temp and RH
+    if (SHT31_ConvertData(MeasurementData, pTempResult, pRHResult) < 1){
+        return -3;
+    }
+
+    return 0;                                           // all went well
+}
+
+int8_t SHT31_HeaterOnOff(uint8_t OnOff){
+    // Switch heater on if OnOff = 1 / off if OnOff = 0
+    // Returns:
+    // 1 if success
+    // -1 if failed or invalid option
+
+    switch(OnOff){
+        case 0:                     // Switch OFF
+            if (I2C1_Write(SHT31_Address,
+                    0xFF,
+                    &CMD_HeaterOff[1],
+                    CMD_HeaterOff[0])
+                    < 0) {
+                return -1;
+            }
+            break;
+        case 1:                     // Switch ON
+            if (I2C1_Write(SHT31_Address,
+                            0xFF,
+                            &CMD_HeaterOn[1],
+                            CMD_HeaterOn[0])
+                            < 0) {
+                return -1;        
+            }
+            break;
+        default:
+            return -1;              // Invalid option
+    }
+    return 1;
+}
+
+int8_t SHT31_GetData(uint8_t MeasurementData[], uint8_t Repeatability){
+    const uint8_t * pCommand;
+    uint8_t MeasurementDuration;
+    switch(Repeatability){
+        case 1:                                 // High repeatability
+            MeasurementDuration = 5;
+            pCommand = &CMD_SSLowRep[0];
+            break;
+        case 2:                                 // High repeatability
+            MeasurementDuration = 7;
+            pCommand = &CMD_SSMidRep[0];
+            break;
+        case 3:                                 // High repeatability
+            MeasurementDuration = 16;
+            pCommand = &CMD_SSHighRep[0];
+            break;
+    }
+       
+    // address device and start measurement
     if(I2C1_Write(SHT31_Address,
                 0xFF,
-                &CMD_SingleShot[1],
-                (char*)CMD_SingleShot[0])
+                pCommand+1,
+                *pCommand)
                 < 0){
         return -1;
     }
     
     // read back data to buffer
-    __delay_ms(100);
+    __delay_ms(MeasurementDuration +2);    
     if(I2C1_Read(SHT31_Address,
                 0xFF,
-                &MeasurementData,
+                MeasurementData,
                 6) < 0){
         return -1;
     }
-    
-    // check CRCs on data
-    
-    // calculate temp and RH
-    SHT31_ConvertData(&MeasurementData, &buffer);
-    
-    return 0;
+    return 1;
 }
 
-int SHT31_ConvertData(unsigned char *MeasurementData, signed int *buffer){
-    unsigned int TempRaw,RHRaw = {0};
-    char AsciiResult[5] = {0};
+int8_t SHT31_CheckCRC(uint8_t DataToCheck[]){
+    // Check CRC for RH and temp
+    // CRC is one byte for two bytes of data
+    // Provide pointer to first byte.
+    // Second value and CRC are expected to follow directly after the first byte
+    // Initial value: 0xFF
+    // Polynomial: 0x31 (x^8 + x^5 + x^4 + 1)
+    // returns 1 if correct
+    // returns 0 if incorrect or issues
+    
+    const uint8_t POLYNOMIAL = 0x31;  // 
+    uint8_t crc = 0xFF;               // initial value
+    uint8_t c = 2;                    // fixed to two bytes input
+    uint8_t i = 8;                    // for 8 bits in each byte
+    
+    for(c=2; c>0; c--){
+        crc ^= (int)*DataToCheck++;              // after the second byte, the pointer is incremented
+                                            // and points to the CRC value that we want to check
+        for (i = 8; i>0;i--){
+            if (crc & 0x80){
+                crc = (crc << 1) ^ POLYNOMIAL;
+            }else{
+                crc = crc << 1;
+            }
+        }
+    }
+    if(crc == *DataToCheck){                    // story checks out
+        return 1;
+    }else{                                      // CRC mismatch
+        return 0;
+    }
+}
+
+int8_t SHT31_ConvertData(  uint8_t MeasurementData[],
+                        int16_t * pTempResult, 
+                        uint8_t * pRHResult){
+    // converts data from raw counts to temp (deg C) and RH (%)
+    
+//    __attribute__((aligned(2))) double TempCelcius = 0;
     double TempCelcius = 0;
+    uint16_t TempRaw,RHRaw = {0};
+    double RH = 0;
     
-    // combine both bytes to get raw temperature value
-    TempRaw = ((int)MeasurementData[0] << 8);
-    TempRaw += MeasurementData[1];
-    
-    itoa(&AsciiResult, TempRaw, 10);
-    SendString(4, "Raw temp counts: ");
-    SendString(4, &AsciiResult);
-    
+//    combine both bytes to get raw temperature value
+//    TempRaw = (MeasurementData[0] << 8);
+//    TempRaw += MeasurementData[1];
+    TempRaw = (MeasurementData[0] << 8) | (MeasurementData[1] & 0xff);
     TempCelcius = TempRaw;
-    TempCelcius *= 175;
     TempCelcius /= 0xFFFF;
+    TempCelcius *= 175;
     TempCelcius -= 45;
+    TempCelcius *= 100;
+    *pTempResult = (int)TempCelcius;
     
-    itoa(&AsciiResult, TempCelcius *100, 10);
-    SendString(4, "\nDegrees Celcius (*100): ");
-    SendString(4, &AsciiResult);
-    SendString(4,"\n\n");
+//    Combine both bytes to get raw relative humidity value
+//    RHRaw = (MeasurementData[3] << 8);
+//    RHRaw += MeasurementData[4];
+    RHRaw = (MeasurementData[3] << 8) | (MeasurementData[4] & 0xff);
+    RH = RHRaw;
+    RH /= 0xFFFF;
+    RH *= 100;
+    *pRHResult = RH;
     
-    
-    return 0;
+    return 1;
 }
-
--- Sensirion_SHT31.h
+++ Sensirion_SHT31.h
@@ -1,10 +1,23 @@
+/*
+ * File:   Sensirion_SHT31.h
+ * Author: dieterv
+ * v0.2
+ * Created on February 3, 2020, 3:09 PM
+ * 
+ * Library for Sensirion SHT31 Temp/Relative humidity sensor
+ * Usage:
+ * - Set I2C address in Sensirion_SHT31.c file (const uint8_t SHT31_Address = 0x45)
+ *      ! Default address with address pin tied to ground is 0x44
+ * - Run SHT31_InitReset
+ * - Run SHT31_SingleShot with pointers to where results should be stored and required repeatability
+ * - Internal heater can be turned on/off with SHT31_HeaterOnOff: 1 to switch on, 0 to switch off
+ */
 
 #ifndef XC_SHT31
 #define	XC_SHT31
 
 #include <xc.h> // include processor files - each processor file is guarded.  
 #include "I2C1.h"
-#include "uart.h"
 
 //--------------------Variables--------------------
 extern char AsciiResult[5];
@@ -12,9 +25,11 @@ extern char AsciiResult[5];
 
 //--------------------Functions--------------------
 
-int SHT31_InitReset (void);
-//int SHT31_SingleShot(char*);
-int SHT31_SingleShot(signed int *);
-int SHT31_ConvertData(unsigned char *, signed int *);
+int8_t SHT31_InitReset (void);                             // high level
+int8_t SHT31_HeaterOnOff(uint8_t);                         // high level
+int8_t SHT31_SingleShot(int16_t *, uint8_t *, uint8_t);    // high level
+int8_t SHT31_GetData(uint8_t [], uint8_t Repeatability);
+int8_t SHT31_CheckCRC(uint8_t []);
+int8_t SHT31_ConvertData(uint8_t[], int16_t *, uint8_t *);
 #endif
 
